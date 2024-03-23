1bd74ca1df0cc292d4528cc460789d6e6bab32da
Madu-J Omenka <madu.omenka@hotmail.com>
2024-03-23 18:32:00 +0000

    Added filter to comment list view

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics, permissions
-from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 from django_restf.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -11,6 +12,8 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
